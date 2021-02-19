4dadfe86886e279c09acc67accc8df9fb1d134fc
Hsiao Yao <tomtogo1260@gmail.com>
2021-02-20 04:54:33 +0800

    :bug: fix mesh transform bugs
    fix mesh transform bugs

--- src/shaders/plane/vertex.glsl
+++ src/shaders/plane/vertex.glsl
@@ -5,9 +5,10 @@ varying vec2 vUv;
 
 void main()
 {
-    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-    modelPosition.z += sin(modelPosition.x * 5.0 - uTime) * 0.05;
+    vec3 newPosition = position;
+    newPosition.z += sin(newPosition.x * 5.0 - uTime) * 0.05;
 
+    vec4 modelPosition = modelMatrix * vec4(newPosition, 1.0);
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectedPosition = projectionMatrix * viewPosition;
 
